e581e64cddedacf980611a55d2daf1afaf34991a
Peter Botsford <petertoddbotsford@gmail.com>
2021-06-24 11:35:25 -0600

    commiting the exercise to the git repository

---
+++ let_and_const_exercise.js
@@ -0,0 +1,18 @@
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+const PI = 3.1415926;
+
+//Quiz
+
+// What is the difference between var and let?
+// you can redeclare var but you cannot with let. var is variable hoised while let is block scoped.
+
+//What is the difference between var and const?
+//you cannot redeclare const.
+
+// What is the difference between let and const?
+// you can reassign let but not const.
+
+//What is hoisting?
+// Hoisting is when the variables (var) are brought to the top of the document and read first so any var that is ran before it's initalized returns undefined.
